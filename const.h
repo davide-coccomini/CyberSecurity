b4fe5a0f1d0d72aef1d677d22fb47aa467535e48
Davide57 <davide5713@gmail.com>
2019-09-17 14:26:26 +0200

    Fix errors

--- const.h
+++ const.h
@@ -4,7 +4,17 @@
 #define MAX_COUNTER 10000000
 #define BUFFER_SIZE 512
 
+#define MAX_FILE_SIZE 4294967296 // 4 giga (2^32)
+
+//for the response to the upload and download commands
+#define FILENAME_NOT_VALID 	100
+#define OK					101
+#define FILE_PRESENT		102
+#define FILE_NOT_PRESENT	103
+#define FILE_TOO_LONG		104
+
 //for connectionStatus variable
 #define	CLIENT_DISCONNECTED 0
 #define CLIENT_CONNECTED 1
 #define	ERROR_IN_CONNECTION 2
+
